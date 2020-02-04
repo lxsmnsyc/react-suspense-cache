bd932811a89aedf3ad0d8c65a5afb5075df362e7
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-02-04 23:02:31 +0800

    Create fetcher-only.ts

---
+++ src/strategies/fetcher-only.ts
@@ -0,0 +1,57 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2020 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2020
+ */
+import {
+  ResourceHandler, StorageRequest, StorageResponse, ResourcePlugin, Fetcher, KeyFactory,
+} from '../types';
+import { fetchData } from '../utils/plugin-handler';
+import SuccessOnlyPlugin from '../plugins/success-only-plugin';
+
+export default class FetcherOnly<T> implements ResourceHandler<T> {
+  private plugins: ResourcePlugin<T>[];
+
+  constructor(plugins: ResourcePlugin<T>[]) {
+    this.plugins = plugins.length === 0
+      ? [new SuccessOnlyPlugin()]
+      : plugins;
+  }
+
+  public async handle(
+    _cacheName: string,
+    _keyFactory: KeyFactory,
+    fetcher: Fetcher<T>,
+    request: StorageRequest,
+  ): Promise<StorageResponse<T>> {
+    const response = await fetchData(
+      fetcher,
+      request,
+      this.plugins,
+    );
+
+    return response;
+  }
+}
